8505a74d9bd80763f91549bbcc5ff8a814c7914f
Mathieu Lecarme <mlecarme@bearstech.com>
2021-07-03 12:37:00 +0200

    ping UDP has now its function.

--- cmd/ping.go
+++ cmd/ping.go
@@ -48,30 +48,35 @@ var pingCmd = &cobra.Command{
 			}
 		}
 
-		// TURN client won't create a local listening socket by itself.
-		conn, err := net.ListenPacket("udp4", "0.0.0.0:0")
-		if err != nil {
-			return err
-		}
-		defer conn.Close()
-
 		turnServerAddr := fmt.Sprintf("%s:%d", host, port)
-		username, password, err := BuildRestPasswor(id, []byte(secret), 1*time.Hour)
+		username, password, err := buildRestPasswor(id, []byte(secret), 1*time.Hour)
 		if err != nil {
 			return err
 		}
+		return pingUDP(turnServerAddr, username, password)
+	},
+}
 
-		cfg := &turn.ClientConfig{
-			STUNServerAddr: turnServerAddr,
-			TURNServerAddr: turnServerAddr,
-			Conn:           conn,
-			Username:       username,
-			Password:       password,
-			Realm:          realm,
-			LoggerFactory:  logging.NewDefaultLoggerFactory(),
-		}
+func pingUDP(turnServerAddr string, username string, password string) error {
 
-		fmt.Printf(`
+	// TURN client won't create a local listening socket by itself.
+	conn, err := net.ListenPacket("udp4", "0.0.0.0:0")
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	cfg := &turn.ClientConfig{
+		STUNServerAddr: turnServerAddr,
+		TURNServerAddr: turnServerAddr,
+		Conn:           conn,
+		Username:       username,
+		Password:       password,
+		Realm:          realm,
+		LoggerFactory:  logging.NewDefaultLoggerFactory(),
+	}
+
+	fmt.Printf(`
 {
 	"username" : "%s",
 	"password" : "%s",
@@ -81,82 +86,84 @@ var pingCmd = &cobra.Command{
 }
 `, username, password, turnServerAddr)
 
-		client, err := turn.NewClient(cfg)
+	client, err := turn.NewClient(cfg)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	// Start listening on the conn provided.
+	err = client.Listen()
+	if err != nil {
+		return err
+	}
+	// Allocate a relay socket on the TURN server. On success, it
+	// will return a net.PacketConn which represents the remote
+	// socket.
+	relayConn, err := client.Allocate()
+	if err != nil {
+		return err
+	}
+	defer relayConn.Close()
+
+	// Set up pinger socket (pingerConn)
+	pingerConn, err := net.ListenPacket("udp4", "0.0.0.0:0")
+	if err != nil {
+		return err
+	}
+	defer pingerConn.Close()
+
+	// Start read-loop on pingerConn
+	go func() {
+		buf := make([]byte, 1600)
+		for {
+			n, from, pingerErr := pingerConn.ReadFrom(buf)
+			if pingerErr != nil {
+				break
+			}
+
+			msg := string(buf[:n])
+			if sentAt, pingerErr := time.Parse(time.RFC3339Nano, msg); pingerErr == nil {
+				rtt := time.Since(sentAt)
+				log.Printf("%d bytes from from %s time=%d ms\n", n, from.String(), int(rtt.Seconds()*1000))
+			}
+		}
+	}()
+
+	// Start read-loop on relayConn
+	go func() {
+		buf := make([]byte, 1600)
+		for {
+			n, from, readerErr := relayConn.ReadFrom(buf)
+			if readerErr != nil {
+				break
+			}
+
+			// Echo back
+			if _, readerErr = relayConn.WriteTo(buf[:n], from); readerErr != nil {
+				break
+			}
+		}
+	}()
+
+	time.Sleep(500 * time.Millisecond)
+
+	// Send 10 packets from relayConn to the echo server
+	for i := 0; i < 10; i++ {
+		msg := time.Now().Format(time.RFC3339Nano)
+		_, err = pingerConn.WriteTo([]byte(msg), relayConn.LocalAddr())
 		if err != nil {
 			return err
 		}
-		defer client.Close()
-		// Start listening on the conn provided.
-		err = client.Listen()
-		if err != nil {
-			return err
-		}
-		// Allocate a relay socket on the TURN server. On success, it
-		// will return a net.PacketConn which represents the remote
-		// socket.
-		relayConn, err := client.Allocate()
-		if err != nil {
-			return err
-		}
-		defer relayConn.Close()
 
-		// Set up pinger socket (pingerConn)
-		pingerConn, err := net.ListenPacket("udp4", "0.0.0.0:0")
-		defer pingerConn.Close()
+		// For simplicity, this example does not wait for the pong (reply).
+		// Instead, sleep 1 second.
+		time.Sleep(time.Second)
+	}
 
-		// Start read-loop on pingerConn
-		go func() {
-			buf := make([]byte, 1600)
-			for {
-				n, from, pingerErr := pingerConn.ReadFrom(buf)
-				if pingerErr != nil {
-					break
-				}
-
-				msg := string(buf[:n])
-				if sentAt, pingerErr := time.Parse(time.RFC3339Nano, msg); pingerErr == nil {
-					rtt := time.Since(sentAt)
-					log.Printf("%d bytes from from %s time=%d ms\n", n, from.String(), int(rtt.Seconds()*1000))
-				}
-			}
-		}()
-
-		// Start read-loop on relayConn
-		go func() {
-			buf := make([]byte, 1600)
-			for {
-				n, from, readerErr := relayConn.ReadFrom(buf)
-				if readerErr != nil {
-					break
-				}
-
-				// Echo back
-				if _, readerErr = relayConn.WriteTo(buf[:n], from); readerErr != nil {
-					break
-				}
-			}
-		}()
-
-		time.Sleep(500 * time.Millisecond)
-
-		// Send 10 packets from relayConn to the echo server
-		for i := 0; i < 10; i++ {
-			msg := time.Now().Format(time.RFC3339Nano)
-			_, err = pingerConn.WriteTo([]byte(msg), relayConn.LocalAddr())
-			if err != nil {
-				return err
-			}
-
-			// For simplicity, this example does not wait for the pong (reply).
-			// Instead, sleep 1 second.
-			time.Sleep(time.Second)
-		}
-
-		return nil
-	},
+	return nil
 }
 
-func BuildRestPasswor(id string, secret []byte, ttl time.Duration) (string, string, error) {
+func buildRestPasswor(id string, secret []byte, ttl time.Duration) (string, string, error) {
 	//https://stackoverflow.com/questions/35766382/coturn-how-to-use-turn-rest-api#54725092
 	if ttl <= 0 {
 		return "", "", errors.New("use a TTL > 0")
